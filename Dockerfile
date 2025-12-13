79a23dbbbc798b2b57fa7218411ce1bf7a89aa21
Prem Prakash <ne.premprakash53@gmail.com>
2025-12-13 15:22:00 +0530

    Clean repo for TerminalBench execution

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.10-slim
-
-WORKDIR /app
-COPY . /app
-
-RUN chmod +x scripts/run.sh
-
-CMD ["sh", "scripts/run.sh"]
--- scripts/solution.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-mkdir -p /app/output
-
-for MAZE_ID in 1 2 3 4 5 6 7 8 9 10
-do
-  python /app/explorer.py $MAZE_ID
-done
